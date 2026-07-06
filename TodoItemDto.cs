5eb093597594fa035f45084e9ae2e9a170e26921
hiratakaiki-impl <hirata.kaiki@impl.co.jp>
2026-07-06 11:14:25 +0900

    過剰な投稿を防止する

---
+++ TodoItemDto.cs
@@ -0,0 +1,10 @@
+public class TodoItemDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+
+    public TodoItemDto() { }
+    public TodoItemDto(Todo todoItem) =>
+    (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
+}
